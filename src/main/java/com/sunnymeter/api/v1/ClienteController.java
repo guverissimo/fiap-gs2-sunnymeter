bfb45102b363023b05cb2e4f369cc37dca5a70a7
Gustavo Verissimo <gustavo.verissimo1@outlook.com>
2024-11-17 23:40:54 -0300

    Client: add functionality to find all clients

--- src/main/java/com/sunnymeter/api/v1/ClienteController.java
+++ src/main/java/com/sunnymeter/api/v1/ClienteController.java
@@ -3,7 +3,9 @@ package com.sunnymeter.api.v1;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +15,11 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
-
 import com.sunnymeter.api.core.cliente.Cliente;
 import com.sunnymeter.api.core.cliente.ClienteRepository;
 import com.sunnymeter.api.core.cliente.DadosCadastroCliente;
 import com.sunnymeter.api.core.cliente.DadosDetalhamentoCliente;
+import com.sunnymeter.api.core.cliente.DadosListagemCliente;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
@@ -41,6 +43,12 @@ public class ClienteController {
 		repository.save(cliente);
 		var uri = uriBuilder.path("/clientes/{id}").buildAndExpand(cliente.getId()).toUri();	
 		return ResponseEntity.created(uri).body(new DadosDetalhamentoCliente(cliente));
+	}
+	
+	@GetMapping
+	public ResponseEntity<Page<DadosListagemCliente>> findAll(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
+		var page = repository.findAll(paginacao).map(DadosListagemCliente::new);
+		return ResponseEntity.ok(page);
 	}
 	
 	@GetMapping("/{cliente_uuid}")
